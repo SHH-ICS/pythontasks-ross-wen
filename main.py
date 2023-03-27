9fb828f976ce0f0c9cd42c72c4f8750160e51167
ross-wen <124378861+ross-wen@users.noreply.github.com>
2023-03-27 10:26:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 # TASK: Update both functions to reverse the letters in the name and provide the square root of the users age.
+import math
 
 def reverseName(myName):
-  # Write your function here
-  result = myName
-  return result
+    myNameReversed = []
+    for x in myName:
+        myNameReversed.insert(0,x)
+    myName = (''.join(myNameReversed))
+    result = myName
+    return result
   
 def rootAge(myAge):
-  # Wrie your function here
-  result = myAge
-  return result
+    myAge = math.sqrt(myAge)
+    result = myAge
+    return result
   
 me = input("What is your name? ")
-im = input("What is your age? ")
+im = float(input("What is your age? "))
 
 print("Your name in reverse is: ",reverseName(me))
 print("The square root of your age is: ",rootAge(im))
